4d44d9fc3b3993506b2c4cd5a3055600372cc5fa
pnh4033 <pnh4033@58.233.130.105>
2017-07-06 16:29:06 +0900

    migration to eclipse oxygen

---
+++ pf_auction/src/main/java/web/portfolio/controller/homeController.java
@@ -0,0 +1,16 @@
+package web.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+	
+	@RequestMapping(value="/")
+	public void home() throws Exception {
+		
+		
+		
+	}
+
+}
