db0259d83ff584446a09501db1d53898200daf97
pnh4033 <pnh4033@58.233.130.105>
2017-09-01 18:19:48 +0900

    폴더 구조 복귀

--- pf_auction/src/main/java/web/portfolio/controller/homeController.java
+++ pf_auction/src/main/java/web/portfolio/controller/homeController.java
@@ -1,9 +1,5 @@
 package web.portfolio.controller;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/desktop2
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -13,10 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class homeController {
 	
 	private static final Logger logger=LoggerFactory.getLogger(homeController.class);
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/desktop2
 	
 	@RequestMapping(value="/")
 	public String home() throws Exception {
